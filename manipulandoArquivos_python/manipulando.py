5671a7538a757e0736f63842d177f60f9a4c7d2a
agostin-afk <agostinho008.af45@gmail.com>
2023-05-04 22:17:45 -0300

    nada

--- manipulandoArquivos_python/manipulando.py
+++ manipulandoArquivos_python/manipulando.py
@@ -6,10 +6,18 @@ for linha in linhas:
     print(linha)
     nomes.append(linha)
 print(nomes)"""
-with open("arquivo.txt") as aqv:
+"""with open("arquivo.txt") as aqv:
     linhas = aqv.readlines()
     nomes = []
     for linha in linhas:
         print(linha)
         nomes.append(linha)
+print(nomes)"""
+
+with open("Teste.txt", "r") as arquivo:
+    linhas = arquivo.readlines()
+    nomes = []
+    for linha in linhas:
+        print(linha)
+        nomes.append(linha.strip())
 print(nomes)
